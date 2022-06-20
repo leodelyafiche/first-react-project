da799c331b1cfbcde1b29f62f9c3e0aaddd11f2c
Leonard <leo.mulyarchik@gmail.com>
2022-06-20 19:19:19 +0300

    repair project commit

---
+++ src/components/app/app.js
@@ -0,0 +1,29 @@
+import './app.css';
+import AppInfo from "../app-info/app-info";
+import SearchPanel from "../search-panel/search-panel";
+import AppFilter from "../app-filter/app-filter";
+import EmployeesList from "../employees-list/employees-list";
+import EmployeesAddForm from "../employees-add-form/employees-add-form";
+
+function App() {
+
+
+    const data = [
+        {name: 'Danny', salary:800, increase: false, id: 1},
+        {name: 'Leo', salary:3000, increase: true, id: 2},
+        {name: 'Sergey', salary:50400, increase: false, id: 3},
+    ];
+
+    return (<div className='app'>
+            <AppInfo/>
+            <div className='search-panel'>
+                <SearchPanel/>
+                <AppFilter/>
+            </div>
+            <EmployeesList data={data}/>
+            <EmployeesAddForm/>
+        </div>)
+
+}
+
+export default App;
